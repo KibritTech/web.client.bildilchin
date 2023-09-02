95af6e9a13d6dc59e22bc964763263d8b7d9dd72
sydvys <aysuseyidova36@gmail.com>
2023-09-02 11:03:00 +0400

    no store

--- app/[locale]/Context/ProjectContext.jsx
+++ app/[locale]/Context/ProjectContext.jsx
@@ -14,7 +14,7 @@ export const ProjectContextProvider = ({ children }) => {
 
   
 const fetchDictionaryList = async () => {
-  const response = await fetch('https://bildilchin.az:8888/bildilchin/get/dictionaryList', { cache: 'force-cache' });
+  const response = await fetch('https://bildilchin.az:8888/bildilchin/get/dictionaryList', { cache: 'no-store' });
  
   if (!response.ok) {
     throw new Error('Failed to fetch data');
